f8879ba806bc330af30a5b6c16b81419d3a5bf06
Jonathan Pobst <monkey@jpobst.com>
2019-07-23 11:28:47 -0500

    OSX backend

---
+++ src/Modern.Forms/Avalonia/Avalonia.Mac/AvaloniaNativePlatformExtensions.cs
@@ -0,0 +1,35 @@
+// Copyright (c) The Avalonia Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using System;
+using Avalonia.Controls;
+using Avalonia.Native;
+
+namespace Avalonia
+{
+    public static class AvaloniaNativePlatformExtensions
+    {
+        //public static T UseAvaloniaNative<T>(this T builder)
+        //    where T : AppBuilderBase<T>, new()
+        //{
+        //    builder.UseWindowingSubsystem(() =>
+        //        AvaloniaNativePlatform.Initialize(
+        //            AvaloniaLocator.Current.GetService<AvaloniaNativePlatformOptions>() ??
+        //            new AvaloniaNativePlatformOptions()));
+        //    return builder;
+        //}
+    }
+
+    public class AvaloniaNativePlatformOptions
+    {
+        public bool UseDeferredRendering { get; set; } = false;
+        public bool UseGpu { get; set; } = true;
+        public string AvaloniaNativeLibraryPath { get; set; }
+    }
+
+    // ReSharper disable once InconsistentNaming
+    public class MacOSPlatformOptions
+    {
+        public bool ShowInDock { get; set; } = true;
+    }
+}
