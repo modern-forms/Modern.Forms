62fbc140e5d0ca79cd22f2914bfcaf6626626975
Jonathan Pobst <monkey@jpobst.com>
2023-07-15 11:56:07 -0500

    [WindowBase] Add key handling events. (#61)

---
+++ samples/ControlGallery/Panels/FormShortcutsPanel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Modern.Forms;
+
+namespace ControlGallery.Panels;
+
+public class FormShortcutsPanel : Panel
+{
+    private readonly TextBox textbox;
+
+    public FormShortcutsPanel (Form form)
+    {
+        textbox = Controls.Add (new TextBox {
+            Left = 10,
+            Top = 10,
+            Width = 200,
+            Height = 400,
+            MultiLine = true,
+        });
+
+        var clear_button = Controls.Add (new Button {
+            Text = "Clear",
+            Left = 220,
+            Top = 10
+        });
+
+        clear_button.Click += (o, e) => textbox.Text = string.Empty;
+
+        form.KeyDown += (s, e) => {
+            textbox.Text += $"KeyDown: '{e.KeyCode}'{Environment.NewLine}";
+            e.Handled = true;
+        };
+
+        form.KeyUp += (s, e) => {
+            textbox.Text += $"KeyUp: '{e.KeyCode}'{Environment.NewLine}";
+            e.Handled = true;
+        };
+
+        form.KeyPress += (s, e) => {
+            textbox.Text += $"KeyPress: '{e.Text}'{Environment.NewLine}";
+            e.Handled = true;
+        };
+    }
+}
---
+++ src/Modern.Forms/WindowBase.Events.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Modern.Forms;
+
+public abstract partial class WindowBase
+{
+    // This pattern is ugly, but it saves allocations
+    // https://docs.microsoft.com/en-us/dotnet/standard/events/how-to-handle-multiple-events-using-event-properties
+    private static readonly object s_keyDownEvent = new object ();
+    private static readonly object s_keyPressEvent = new object ();
+    private static readonly object s_keyUpEvent = new object ();
+
+    /// <summary>
+    /// Raised when the user presses down a key.
+    /// </summary>
+    public event EventHandler<KeyEventArgs>? KeyDown {
+        add => Events.AddHandler (s_keyDownEvent, value);
+        remove => Events.RemoveHandler (s_keyDownEvent, value);
+    }
+
+    /// <summary>
+    /// Raised when the user presses a key.
+    /// </summary>
+    public event EventHandler<KeyPressEventArgs>? KeyPress {
+        add => Events.AddHandler (s_keyPressEvent, value);
+        remove => Events.RemoveHandler (s_keyPressEvent, value);
+    }
+
+    /// <summary>
+    /// Raised when the user releases a key.
+    /// </summary>
+    public event EventHandler<KeyEventArgs>? KeyUp {
+        add => Events.AddHandler (s_keyUpEvent, value);
+        remove => Events.RemoveHandler (s_keyUpEvent, value);
+    }
+
+    /// <summary>
+    /// Raises the KeyDown event.
+    /// </summary>
+    protected virtual void OnKeyDown (KeyEventArgs e) => (Events[s_keyDownEvent] as EventHandler<KeyEventArgs>)?.Invoke (this, e);
+
+    /// <summary>
+    /// Raises the KeyPress event.
+    /// </summary>
+    protected virtual void OnKeyPress (KeyPressEventArgs e) => (Events[s_keyPressEvent] as EventHandler<KeyPressEventArgs>)?.Invoke (this, e);
+
+    /// <summary>
+    /// Raises the KeyUp event.
+    /// </summary>
+    protected virtual void OnKeyUp (KeyEventArgs e) => (Events[s_keyUpEvent] as EventHandler<KeyEventArgs>)?.Invoke (this, e);
+}
