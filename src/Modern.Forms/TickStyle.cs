fd1cf10cd97ae473a2d09879e58ba971d4205aeb
Kiko kiko <ssasske08@gmail.com>
2026-04-03 16:54:14 +0200

    Add TrackBar control with renderer and demo panel

---
+++ src/Modern.Forms/TickStyle.cs
@@ -0,0 +1,30 @@
+﻿namespace Modern.Forms
+{
+    /// <summary>
+    /// Specifies where tick marks are drawn on a <see cref="TrackBar"/>.
+    /// </summary>
+    public enum TickStyle
+    {
+        /// <summary>
+        /// No tick marks are displayed.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Tick marks are displayed on the top side of a horizontal <see cref="TrackBar"/>
+        /// or on the left side of a vertical <see cref="TrackBar"/>.
+        /// </summary>
+        TopLeft,
+
+        /// <summary>
+        /// Tick marks are displayed on the bottom side of a horizontal <see cref="TrackBar"/>
+        /// or on the right side of a vertical <see cref="TrackBar"/>.
+        /// </summary>
+        BottomRight,
+
+        /// <summary>
+        /// Tick marks are displayed on both sides of the track.
+        /// </summary>
+        Both
+    }
+}
